839dba7c7e942b4f8906447da77fa10c93a3c093
jasminelopez <lopez.jasmine094@gmail.com>
2019-05-08 19:46:52 -0700

    Add masterForm class component, and step child components -  streamline babel tranpilation process

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,79 @@
 
+class MAsterForm extends React.Component {
+  render () {
+    return 
+      <h1>Hello! This is a test for Babel!</h1>
+      <button action="/nameform" method="get"></button>
+  }
+}
 
+class Step1 extends React.Component {
+  render() {
+    if (this.props.currentStep !== 1) { // Prop: The current step
+      return null
+    }
+    // The markup for the Step 1 UI
+    return(
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          className="form-control"
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Enter email"
+          value={this.props.email} // Prop: The email input data
+          onChange={this.props.handleChange} // Prop: Puts data into state
+        />
+      </div>
+    )
+  }
+}
+
+class Step2 extends React.Component {
+  render() {
+    if (this.props.currentStep !== 1) { // Prop: The current step
+      return null
+    }
+    // The markup for the Step 1 UI
+    return(
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          className="form-control"
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Enter email"
+          value={this.props.email} // Prop: The email input data
+          onChange={this.props.handleChange} // Prop: Puts data into state
+        />
+      </div>
+    )
+  }
+}
+
+class Step3 extends React.Component {
+  render() {
+    if (this.props.currentStep !== 1) { // Prop: The current step
+      return null
+    }
+    // The markup for the Step 1 UI
+    return(
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          className="form-control"
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Enter email"
+          value={this.props.email} // Prop: The email input data
+          onChange={this.props.handleChange} // Prop: Puts data into state
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<MasterForm />, document.getElementById('root'));
