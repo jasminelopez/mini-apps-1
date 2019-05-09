1f018dfc92c6d08b8e81d648104f0ed97c2381be
jasminelopez <lopez.jasmine094@gmail.com>
2019-05-09 15:22:00 -0700

    make changes to file names for ease of use - create server on port 3000 - add the static html file as the beginning page - move html file to public directory

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,9 @@
 
+const express = require('express');
+const app = express();
+const port = 3000;
 
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Connect four app listening on port ${port}!!`));
